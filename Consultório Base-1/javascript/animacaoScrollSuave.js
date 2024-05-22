6170422af45b9ca7c6458681094c42c27453f845
Gilmar1212 <50801730+Gilmar1212@users.noreply.github.com>
2024-05-22 00:06:48 -0300

    Delete Consultório Base-1 directory

--- Consultório Base-1/javascript/animacaoScrollSuave.js
+++
@@ -1,8 +0,0 @@
-$("nav a").click(function(e){
-	e.PreventDefault;
-	var id =$(this).attr('href');
-	targetOffset=$(id).offset().top;
-	$('html,body').animate({
-		scrollTop:targetOffset-220
-	},500);
-});
